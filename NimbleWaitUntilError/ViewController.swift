c929fa092549873401dcadcc1759c4e6ee9a2ee8
Nikita Leonov <nikita@buddyhopp.com>
2014-10-31 19:31:40 -0700

    Initial Commit

---
+++ NimbleWaitUntilError/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NimbleWaitUntilError
+//
+//  Created by Nikita Leonov on 10/31/14.
+//  Copyright (c) 2014 BuddyHOPP SA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
